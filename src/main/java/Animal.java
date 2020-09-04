0409928f6016bd8dcec8d3ea805cc15b580c055f
kaiwen98 <e0406836@u.nus.edu>
2020-09-04 17:24:23 +0800

    Added these changes in lecture

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,6 +1,8 @@
-public class Animal {
-    
-    public String speak(){
-        return null;
+public abstract class Animal implements Asset{
+    private int value = 10;
+    public abstract String speak();
+    @Override
+    public int getValue(){
+        return value;
     }
 }
--- src/main/java/Equipment.java
+++ src/main/java/Equipment.java
@@ -1,10 +1,11 @@
-public abstract class Equipment {
+public abstract class Equipment implements Asset{
     protected int purchasePrice;
 
     public Equipment(int purchasePrice) {
         this.purchasePrice = purchasePrice;
     }
 
+    @Override
     public int getValue() {
         return purchasePrice;
     }
